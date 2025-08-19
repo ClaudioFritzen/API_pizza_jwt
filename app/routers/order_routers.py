e48df6c44b30b2da0f30067b5dc56717fcfd85c0
ClaudioFritzen <sdfritzen96@gmail.com>
2025-08-19 14:53:26 +0100

    feat - criamos o schema de pedidos e a rota para pedidos

--- app/routers/order_routers.py
+++ app/routers/order_routers.py
@@ -1,7 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+
+from app.models.models import Pedido
+## router
+from app.db.session import get_db
+from sqlalchemy.orm import Session as SessionType 
+
+from app.schemas.schema_pedidos import PedidoSchema
 
 order_router = APIRouter(prefix="/pedidos", tags=["pedidos"])
 
 @order_router.post("/")
-async def create_order(item_id: int, quantity: int, nome: str):
-    return {"item_id": item_id, "quantity": quantity, "nome": nome}
+async def pedidos():
+    """
+    Essa é a rota padrao de pedidos do nosso sistema. Todas as rotas dos pedidos precisam de autenticação.
+
+    """
+    return {"message": "Você acessou a rota pedidos"}
+
+
+@order_router.post("/criar_pedido")
+async def criar_pedido(pedido_schema: PedidoSchema, db: SessionType = Depends(get_db)):
+    """
+    Essa é a rota para criar um novo pedido.
+    """
+    if not pedido_schema.usuario_id:
+        raise HTTPException(status_code=400, detail="ID do usuário é obrigatório")
+
+    novo_pedido = Pedido(**pedido_schema.dict())
+
+    db.add(novo_pedido)
+    db.commit()
+    return {"message": f"Pedido criado com sucesso para o usuário {pedido_schema.usuario_id}"}
---
+++ app/schemas/schema_pedidos.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from pydantic import ConfigDict
+
+class PedidoSchema(BaseModel):
+    usuario_id: int
+
+    model_config = ConfigDict(from_attributes=True)
