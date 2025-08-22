046031965dd35ab1f15252507d899d3b51e90384
ClaudioFritzen <sdfritzen96@gmail.com>
2025-08-22 18:57:06 +0100

    chore: remover rota antiga e limpar imports após migração para auth_router

--- app/main.py
+++ app/main.py
@@ -2,14 +2,8 @@ from fastapi import FastAPI
 from app.routers.auth_routers import auth_router
 from app.routers.order_routers import order_router
 
-## classes para tests
-from app.routers import usuarios
-
-
 app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(order_router)
 
-# nossa rota de testes do usuarios
-app.include_router(usuarios.router)
