5ba0a21ce688076d0ea4701512fc92d8236b82d5
ClaudioFritzen <sdfritzen96@gmail.com>
2025-08-28 17:05:14 +0100

    fix (pydantic): - modificamos o decorador @validador que esta esta obsoleto e mudados para o da nova versao @field_validator

--- app/schemas/schema_pedidos.py
+++ app/schemas/schema_pedidos.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, field_validator
 from pydantic import ConfigDict
 
 
 class PedidoSchema(BaseModel):
     usuario_id: int
 
-    @validator("usuario_id")
+    @field_validator("usuario_id")
     def validar_usuario_id(cls, v):
         if v <= 0:
             raise ValueError("O ID do usuário deve ser um número positivo.")
